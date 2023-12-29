7a7fb805a1b526b14bc7f6bab05ded5f6c20c541
Hayden Crabb <haydencrabb@icloud.com>
2023-12-29 12:33:13 -0700

    Name Change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: "Rovin' Ranger",
+  appName: "Roamin' Ranger",
   webDir: 'www',
   server: {
     androidScheme: 'https'
